28e2c6557bc9d8c1a4bab062e730913b806a82e5
Louis RREINE <louis.reine@eleves.enpc.fr>
2021-03-25 14:40:14 +0100

    changed going front to be able to regain control when the drone is under the control of the raspberry pi

--- test-scripts/test-going-front.py
+++ test-scripts/test-going-front.py
@@ -1,11 +1,19 @@
 from dronekit import connect, VehicleMode
 from pymavlink import mavutil
 import time
+import logging
 
 # connection to drone, using tcp
-
 print 'Connecting to drone'
-inspection_drone = connect('tcp:127.0.0.1:5762', wait_ready=True)
+inspection_drone = connect('/dev/serial0', wait_ready=True, baud=921600)
+
+# Log handling. Logs are saved in test-going-front.py
+
+
+logging.basicConfig(format='%(asctime)s %(message)s')
+logging.basicConfig(filename='test-going-front.log', level=logging.DEBUG)
+
+logging.info('test-going-front started')
 
 
 # Function taken from the dronekit documentation :
@@ -25,25 +33,62 @@ def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
         0, 0, 0,  # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
         0, 0)  # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
 
-    # send command to vehicle on 1 Hz cycle
-    for x in range(0, duration):
-        inspection_drone.send_mavlink(msg)
-        time.sleep(1)
+    print "SENDING MAVLINK SIGNAL : GOING BACKWARD"
+    inspection_drone.send_mavlink(msg)
 
 
-def test_go_forward():
+def test_go_forward(drone, speed, test_time):
     # Printing the drone current flight mode.
-    # If the drone is passed in GUIDED flight mode, it goes forward for 5s at 0.3m/s.
+    # When the drone is passed in GUIDED flight mode, it executes the following instructions :
+    # stay stationary during 4 seconds
+    # goes forward seconds at 0.3m/s during test_time
+    # stop and stay stationary during 4 seconds
     # Try to see if you can regain control at every moment
+    current_flight_mode = drone.mode
     is_being_tested = True
+    launch_test = False
+    total_test_time = test_time + 4 * 2
+    print_time = 0
     while True:
-        print inspection_drone.mode
-        if inspection_drone.mode == VehicleMode('GUIDED') and is_being_tested:
-            print 'DRONE IS MOVING AUTONOMOUSLY'
-            send_ned_velocity(-0.3, 0, 0, 5)
-            is_being_tested = False
-        time.sleep(1)
+        current_time = time.time()
+
+        # Check when we first go into guided mode
+        if current_flight_mode != drone.mode:
+            if drone.mode == VehicleMode('GUIDED'):
+                launch_test = True
+                test_launch_time = time.time()
+                logging.warn("LAUNCH THE TEST")
+                print "LAUNCHED"
+            current_flight_mode = drone.mode
+
+        if (time.time() - print_time) > 2:
+            print drone.mode
+            print_time = time.time()
+
+        if drone.mode == VehicleMode('GUIDED') and launch_test and is_being_tested:
+            time_elapsed = current_time - test_launch_time
+
+            # Wait still for 4 seconds
+            if time_elapsed < 4:
+                logging.info("We wait for 4 seconds")
+                send_ned_velocity(0, 0, 0)
+
+            # Perform the action for test_time
+            if 4 < time_elapsed < 4 + test_time:
+                logging.info("We go forward for %s seconds" % test_time)
+                send_ned_velocity(speed, 0, 0)
+
+            # Wait still for 4 seconds after the action
+            if 4 + test_time < time_elapsed < total_test_time:
+                logging.info("We wait for 4 seconds after performing the orders")
+                send_ned_velocity(0, 0, 0)
+
+            # If the test has been done, reset all parameters.
+            if time_elapsed > total_test_time:
+                logging.warn("TEST DONE")
+                launch_test = False
+                is_being_tested = False
 
 
 print 'launching test-going-forward'
-test_go_forward()
+test_go_forward(inspection_drone, 0.1, 3)
---
+++ test-scripts/test-regain-control.py
@@ -0,0 +1,21 @@
+from dronekit import connect, VehicleMode
+from pymavlink import mavutil
+import time
+
+# connection to drone, using tcp
+
+print 'Connecting to drone'
+inspection_drone = connect('/dev/serial0', wait_ready=True)
+
+def test_regain_control():
+    # Printing the drone current flight mode.
+    # If the drone is passed in GUIDED flight mode, it will print something else and
+    # Try to see if you can regain control at every moment
+    is_being_tested = True
+    while True:
+        print inspection_drone.mode
+        if inspection_drone.mode == VehicleMode('GUIDED') and is_being_tested:
+            print 'DRONE IS MOVING AUTONOMOUSLY'
+        time.sleep(1)
+
+test_regain_control()
