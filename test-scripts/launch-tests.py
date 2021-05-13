6b6a6ae7fb034694d95fea18e3e84897bc88fb92
Louis RREINE <louis.reine@eleves.enpc.fr>
2021-05-13 15:23:24 +0200

    updated flying-test-checklist

--- test-scripts/launch-tests.py
+++ test-scripts/launch-tests.py
@@ -2,8 +2,9 @@ from dronekit import connect, VehicleMode
 from pymavlink import mavutil
 import time
 import RPi.GPIO as GPIO
+from RangeSensors import *
 
-
+print "i work"
 # Select GPIO mode
 GPIO.setmode(GPIO.BCM)
 # Set buzzer - pin 23 as output
@@ -13,7 +14,7 @@ GPIO.setup(buzzer, GPIO.OUT)
 # connection to drone, using serial
 print 'Connecting to drone'
 inspection_drone = connect('/dev/serial0', wait_ready=True, baud=115200)
-
+print 'Successfully connected to drone !'
 # ------------------
 # Parameters
 # ------------------
@@ -27,18 +28,20 @@ inspection_drone.timeLastObstacleDetected = 0
 print_time = time.time()
 
 # Speed and time going forward for test 1 : going forward
-speedTest1 = 0.1
+speedTest1 = 1
 timeTest1 = 4
 
 # Speed and side_length for test 2 : following a square pattern
-speedTest2 = 0.1
-sideLength = 1
+speedTest2 = 1
+sideLength = 5
 timeSide = sideLength / speedTest2
 totalTime = 4 * sideLength / speedTest2
 
 # Speed and side_length for test 3 : Auto mode then Guided and stop when there is an obstacle.
-SpeedTest3 = 0.1
-timeTest3 = 4
+use_sonar = False
+use_lidar = True
+sonar = Sonar(200)
+lidar = Lidar(300)
 
 
 # Management of RC Channels to launch code
@@ -90,26 +93,28 @@ def RC_CHANNEL_listener(vehicle, name, message):
     if message.chan8_raw > 1500:
         if vehicle.testNumber == -1 and (time.time() - vehicle.startTime) > 0.4:
             print("Launching code !")
-            vehicle.mode = VehicleMode("GUIDED")
             vehicle.startTime = time.time()
             if 0 < message.chan6_raw < 1200:
                 vehicle.testNumber = 0
+                vehicle.mode = VehicleMode("GUIDED")
             if 1200 < message.chan6_raw < 1800:
                 vehicle.testNumber = 1
+                vehicle.mode = VehicleMode("GUIDED")
             if 1800 < message.chan6_raw < 2000:
                 vehicle.testNumber = 2
         else:
             vehicle.testNumber = -1
             print("Stopped Test using Transmitter input")
 
-def send_ned_velocity(drone, velocity_x, velocity_y, velocity_z):
+
+def send_local_frame_offset_velocity(drone, velocity_x, velocity_y, velocity_z):
     """
     Move vehicle in direction based on specified velocity vectors.
     """
     msg = drone.message_factory.set_position_target_local_ned_encode(
         0,  # time_boot_ms (not used)
         0, 0,  # target system, target component
-        mavutil.mavlink.MAV_FRAME_LOCAL_NED,  # frame
+        mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED,  # frame
         0b0000111111000111,  # type_mask (only speeds enabled)
         0, 0, 0,  # x, y, z positions (not used)
         velocity_x, velocity_y, velocity_z,  # x, y, z velocity in m/s
@@ -121,27 +126,27 @@ def send_ned_velocity(drone, velocity_x, velocity_y, velocity_z):
 
 def send_mavlink_go_forward(drone, velocity):
     print "Going forward"
-    send_ned_velocity(drone, velocity, 0, 0)
+    send_local_frame_offset_velocity(drone, velocity, 0, 0)
 
 
 def send_mavlink_go_left(drone, velocity):
     print "Going left"
-    send_ned_velocity(drone, 0, -velocity, 0)
+    send_local_frame_offset_velocity(drone, 0, -velocity, 0)
 
 
 def send_mavlink_go_right(drone, velocity):
     print "Going right"
-    send_ned_velocity(drone, 0, velocity, 0)
+    send_local_frame_offset_velocity(drone, 0, velocity, 0)
 
 
 def send_mavlink_go_backward(drone, velocity):
     print "Going backward"
-    send_ned_velocity(drone, -velocity, 0, 0)
+    send_local_frame_offset_velocity(drone, -velocity, 0, 0)
 
 
 def send_mavlink_stay_stationary(drone):
     print "Stopping"
-    send_ned_velocity(drone, 0, 0, 0)
+    send_local_frame_offset_velocity(drone, 0, 0, 0)
 
 
 def print_no_spam(start_print_time, string):
@@ -157,7 +162,23 @@ def is_in_guided_mode(drone):
     return drone.mode == VehicleMode("GUIDED")
 
 
+def obstacle_Detected(mySonar, myLidar, use_lidar, use_sonar, debug=False):
+    """ Function that takes as input two objects of type sonar and lidar
+    and that returns True if the distance they read is inferior to the critical distance defined for each of them
+    The param debug has to be set to True if you want the data to be printed when uptdated"""
+    read_serial = readSensorsLine()
+    if mySonar.read_distance(read_serial) and debug:
+        print "Sonar range:" + str(mySonar.get_distance())
+    if myLidar.read_distance(read_serial) and debug:
+        print "Lidar range:" + str(myLidar.get_distance())
+    if (mySonar.critical_Distance_Reached() and use_sonar) or (use_lidar and myLidar.critical_Distance_Reached()):
+        return True
+    return False
+
+
 while True:
+
+    inspection_drone.obstacleDetected = obstacle_Detected(sonar, lidar, use_lidar, use_sonar)
 
     if inspection_drone.testNumber == 0 and is_in_guided_mode(inspection_drone):
 
@@ -195,7 +216,7 @@ while True:
 
         if 3 * timeSide < elapsed_time < 4 * timeSide:
 
-            send_mavlink_go_forward(inspection_drone, speedTest2)
+            send_mavlink_go_right(inspection_drone, speedTest2)
 
         elif 4 * timeSide < elapsed_time:
 
@@ -208,28 +229,26 @@ while True:
         print(elapsed_time)
         print(inspection_drone.mode)
 
-        if is_in_guided_mode(inspection_drone) and not inspection_drone.obstacleDetected:
-            if time.time() - inspection_drone.timeLastObstacleDetected > 2:
-                inspection_drone.mode = VehicleMode("AUTO")
-            else:
-                send_mavlink_stay_stationary(inspection_drone)
-
-        if is_in_guided_mode(inspection_drone) and inspection_drone.obstacleDetected:
+        if is_in_guided_mode(inspection_drone):
             send_mavlink_stay_stationary(inspection_drone)
 
-        if is_in_auto_mode(inspection_drone):
-            if inspection_drone.obstacleDetected:
+        if inspection_drone.obstacleDetected:
+            if not is_in_guided_mode(inspection_drone):
                 inspection_drone.mode = VehicleMode("GUIDED")
-                send_mavlink_stay_stationary(inspection_drone)
+                inspection_drone.testNumber = -1
+
+            if int(time.time() * 2) % 2 == 0:
+                GPIO.output(buzzer, GPIO.HIGH)
+            if int(time.time() * 2) % 2 == 1:
+                GPIO.output(buzzer, GPIO.LOW)
+        else:
+            GPIO.output(buzzer, GPIO.LOW)
 
     print_no_spam(print_time, 'actual test is : %s ' % inspection_drone.selectedTest)
 
     if inspection_drone.testNumber != -1:
         print "Test %s is running" % inspection_drone.testNumber
-        if int(time.time() * 2 ) % 2 == 0:
-            GPIO.output(buzzer, GPIO.HIGH)
-        if int(time.time() * 2) % 2 == 1:
-            GPIO.output(buzzer, GPIO.LOW)
+
     else:
         GPIO.output(buzzer, GPIO.LOW)
 
